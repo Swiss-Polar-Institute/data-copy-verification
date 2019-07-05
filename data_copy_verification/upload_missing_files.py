2b411b7c5088ad5a9632eadd287af54ce0b5e2ac
Carles Pina i Estany <carles@pina.cat>
2019-07-05 17:17:57 +0200

    Improve tool to upload missing files.

--- data_copy_verification/upload_missing_files.py
+++ data_copy_verification/upload_missing_files.py
@@ -4,6 +4,8 @@ import argparse
 import subprocess
 import datetime
 import os
+import sys
+import time
 
 # This was used only once to upload a series of files. It has not been tested more than that case.
 # In normal circumstances better avoid using it and upload the files using another software, use
@@ -16,42 +18,57 @@ def write_to_log(cmd, exit_code):
     f.close()
 
 
-def upload_files(missing_files_file, s3_bucket):
+def upload_files(missing_files_file, local_base_directory, remote_base_directory, s3_bucket):
     f = open(missing_files_file, "r")
 
     lines = f.readlines()
 
     total_number_lines = len(lines)
 
+    start_time = time.time()
+
     count = 0
     for line in lines:
         count += 1
         line = line.rstrip()
-        (file_name, size, hash) = line.split("\t")
+        file_name = line.split("\t")[0]
 
-        local_file_name = "/volume1/{}".format(file_name)
+        if file_name.startswith("./"):
+            file_name = file_name[2:]
 
-        print("Is going to upload file {} of {}".format(count, total_number_lines))
+        local_file_name = os.path.join(local_base_directory, file_name)
+
+        print("Is going to upload file {} of {} - elapsed time: {} minutes".format(count, total_number_lines, (time.time() - start_time) / 60))
 
         if os.path.isdir(local_file_name):
             print("Skips because it is a directory")
             write_to_log("No command for {} is a directory".format(file_name), "0")
             continue
 
-        cmd = ["aws", "s3", "cp", "--endpoint-url=https://s3.epfl.ch", local_file_name, "s3://{}/{}".format(s3_bucket, file_name)]
-        print(cmd)
+        remote_file_name = os.path.join(remote_base_directory, file_name)
+        cmd = ["aws", "s3", "cp", "--endpoint-url=https://s3.epfl.ch", local_file_name, "s3://{}/{}".format(s3_bucket, remote_file_name)]
+
         exit_code = subprocess.call(cmd)
+
         write_to_log(cmd, exit_code)
+
+        if exit_code != 0:
+            print("Error, exit code:", exit_code)
+            sys.exit(1)
+
+    print("Total time: {} minutes".format((time.time() - start_time) / 60))
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("missing_files_file", help="Missing files (3 columns with tabs, first column is the file name)")
+    parser.add_argument("missing_files_file", help="Missing files list (one file per line, can have tabs only first column is used)")
+    parser.add_argument("local_base_directory", help="Local base directory - it's prepended on each line of the missing_files_file line, can be empty")
+    parser.add_argument("remote_base_directory", help="Remote (S3) base directory where to upload the files")
     parser.add_argument("s3_bucket", help="s3_bucket to upload")
 
     args = parser.parse_args()
 
-    upload_files(args.missing_files_file, args.s3_bucket)
+    upload_files(args.missing_files_file, args.local_base_directory, args.remote_base_directory, args.s3_bucket)
 
 
 if __name__ == "__main__":
